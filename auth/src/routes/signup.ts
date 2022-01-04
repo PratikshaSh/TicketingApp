70103db202daf9ce25d46930e0494e4dc1c70e3d
PratikshaSh <pratiksha8520@gmail.com>
2022-01-04 23:34:10 +0530

    Subclassing for Custom Errors

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response} from 'express';
 import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+
+
 
 const router = express.Router();
 
@@ -15,13 +19,26 @@ router.post('/api/users/signup', [
 (req: Request, res: Response) => {
     const errors = validationResult(req);
 
+    
+
     if(!errors.isEmpty()) {
-        return res.status(400).send(errors.array())
+
+        // //This is javascript land ( not ts )
+        // const error = new Error('Invalid Email or Password');
+        // error.reasons = errors.array();
+        // throw error;
+        
+        // throw new Error('Invalid email or password');
+        throw new RequestValidationError(errors.array());
+        
     }
 
-    const { email, password } = req.body;
+    // const { email, password } = req.body;
 
     console.log('Creating a user...');
+    // throw new Error('Error connecting to database');
+    throw new DatabaseConnectionError();
+    
     res.send({});
 
 });
