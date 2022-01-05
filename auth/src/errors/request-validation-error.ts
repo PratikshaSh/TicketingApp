3fbc5f39b4f9a0f11bb5324a81fa420de5f9e771
PratikshaSh <pratiksha8520@gmail.com>
2022-01-05 18:40:01 +0530

    Final error related code

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,11 +1,12 @@
+import { CustomError } from "./custom-error";
 import { ValidationError } from "express-validator";
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
     
     statusCode = 400;
     
     constructor(public errors: ValidationError[]){
-        super();
+        super('Invalid request parrameters');
 
         // Only because we are extending built in class
         Object.setPrototypeOf(this, RequestValidationError.prototype);
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,17 +1,13 @@
 import { NextFunction, Request, Response} from "express";
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
-
+import { CustomError } from "../errors/custom-error";
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
 
     // console.log('Something went wrong', err);
-    if( err instanceof RequestValidationError){
+    if( err instanceof CustomError){
         return res.status(err.statusCode).send({errors: err.serializeErrors()});
     }
 
-    if( err instanceof DatabaseConnectionError){
-        return res.status(err.statusCode).send({ errors : err.serializeErrors() });
-    }
+ 
     res.status(400).send({
         errors: [{ message: 'Something went wrong'}]
     });
